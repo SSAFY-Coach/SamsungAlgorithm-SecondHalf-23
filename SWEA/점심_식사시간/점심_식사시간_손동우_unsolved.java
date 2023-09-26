f1028c5bf5e1fa76625556995c1479068ae0627e
samsohn0128 <samsohn0128@gmail.com>
2023-09-26 17:13:54 +0900

    #5 fix: unsolved 점심_식사시간 (테케 46/50, 주석 추가)

--- SWEA/점심_식사시간/점심_식사시간_손동우_unsolved.java
+++ SWEA/점심_식사시간/점심_식사시간_손동우_unsolved.java
@@ -7,16 +7,14 @@ import java.util.stream.Collectors;
 public class Solution {
     private static int T;
     private static int N;
-    private static List<Person> peopleList = new ArrayList<>();
+    private static final List<Person> peopleList = new ArrayList<>();
     private static Steps steps1;
     private static Steps steps2;
     private static final Comparator<Person> personComparator = Comparator.comparingInt(person -> person.arrivalTime);
-    private static PriorityQueue<Person> stepsPq1 = new PriorityQueue<>(personComparator);
-    private static PriorityQueue<Person> stepsPq2 = new PriorityQueue<>(personComparator);
+    private static final PriorityQueue<Person> stepsPq1 = new PriorityQueue<>(personComparator);
+    private static final PriorityQueue<Person> stepsPq2 = new PriorityQueue<>(personComparator);
     private static int[][] moveTimes;
-    /*
-    moveTimes의 행은 1번 계단으로 들어가는 사람의 수, 열은 2번 계단으로 들어가는 사람의 수
-     */
+    // moveTimes의 행은 1번 계단으로 들어가는 사람의 수, 열은 2번 계단으로 들어가는 사람의 수
 
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("input.txt"));
@@ -34,9 +32,8 @@ public class Solution {
         steps1 = null;
         steps2 = null;
         N = Integer.parseInt(br.readLine());
-        StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 int num = Integer.parseInt(st.nextToken());
                 if (num == 1) {
@@ -54,54 +51,80 @@ public class Solution {
     }
 
     private static int solution() {
+        sortPeopleList();
+
         for (int i = 0; i <= peopleList.size(); i++) {
             putAllPeopleInStepsPq(i);
-            int moveTime1 = calculateMoveTime(stepsPq1, steps1.length);
-            int moveTime2 = calculateMoveTime(stepsPq2, steps2.length);
+            int moveTime1 = calculateMoveTime(stepsPq1, steps1.length); // 계단 1의 총 이동 시간
+            int moveTime2 = calculateMoveTime(stepsPq2, steps2.length); // 계단 2의 총 이동 시간
             moveTimes[i][peopleList.size() - i] = Math.max(moveTime1, moveTime2);
+            //1번 계단에 i명, 2번 계단에 peopleList.size() - i명
         }
+
         int answer = Integer.MAX_VALUE;
         for (int i = 0; i <= peopleList.size(); i++) {
-            for (int j = 0; j <= peopleList.size(); j++) {
-                if (moveTimes[i][j] > 0) {
-                    answer = Math.min(answer, moveTimes[i][j]);
-                }
-            }
+            answer = Math.min(answer, moveTimes[i][peopleList.size() - i]);
         }
         return answer;
     }
 
+    private static void sortPeopleList() {
+        //steps1까지의 거리가 가까운 기준으로 정렬, 같으면 steps2까지의 거리가 먼 기준
+        peopleList.sort((person1, person2) -> {
+            int distance11 = getDistanceToSteps(person1, steps1);
+            int distance21 = getDistanceToSteps(person2, steps1);
+            if (distance11 != distance21) {
+                return distance11 - distance21;
+            } else {
+                int distance12 = getDistanceToSteps(person1, steps2);
+                int distance22 = getDistanceToSteps(person2, steps2);
+                return distance22 - distance12;
+            }
+        });
+    }
+
     private static void putAllPeopleInStepsPq(int numberOfPeopleInSteps1) {
-        peopleList.sort(Comparator.comparingInt(person -> getDistanceToSteps(person, steps1)));
-        stepsPq1.addAll(peopleList.subList(0, numberOfPeopleInSteps1).stream()
-                .peek(person -> {
-                    person.arrivalTime = getDistanceToSteps(person, steps1);
-                    person.startMovingTime = 0;
-                })
-                .collect(Collectors.toList()));
-        stepsPq2.addAll(peopleList.subList(numberOfPeopleInSteps1, peopleList.size()).stream()
-                .peek(person -> {
-                    person.arrivalTime = getDistanceToSteps(person, steps2);
-                    person.startMovingTime = 0;
-                })
-                .collect(Collectors.toList()));
+        stepsPq1.addAll(
+                peopleList.subList(0, numberOfPeopleInSteps1).stream()
+                        .peek(person -> {
+                            person.arrivalTime = getDistanceToSteps(person, steps1);
+                            person.startMovingTime = 0;
+                        })
+                        .collect(Collectors.toList())
+        );
+        stepsPq2.addAll(
+                peopleList.subList(numberOfPeopleInSteps1, peopleList.size()).stream()
+                        .peek(person -> {
+                            person.arrivalTime = getDistanceToSteps(person, steps2);
+                            person.startMovingTime = 0;
+                        })
+                        .collect(Collectors.toList())
+        );
     }
 
     private static int calculateMoveTime(PriorityQueue<Person> stepsPq, int stepsLength) {
         Queue<Person> movingQueue = new LinkedList<>();
         Queue<Person> waitingQueue = new LinkedList<>();
         int currentTime = 0;
-        while (!stepsPq.isEmpty() || !movingQueue.isEmpty() || !waitingQueue.isEmpty()) {
-            currentTime++;
 
+        while (!stepsPq.isEmpty() || !movingQueue.isEmpty() || !waitingQueue.isEmpty()) {
+            currentTime++; //시간이 1씩 흐른다.
+
+            //계단에서 이동 중인 사람이 도착할 때가 되면 movingQueue 에서 pop
             while (!movingQueue.isEmpty() && movingQueue.peek().startMovingTime + stepsLength <= currentTime) {
                 movingQueue.poll();
             }
+
+            //이미 계단 입구에 도착해서 기다리고 있는 사람이 있고, 계단에 이동 중인 사람이 3명 미만이면
+            // waitingQueue 에서 pop 해서 movingQueue 에 push
             while (!waitingQueue.isEmpty() && movingQueue.size() < 3) {
                 Person movingPerson = waitingQueue.poll();
                 movingPerson.startMovingTime = currentTime;
                 movingQueue.offer(movingPerson);
             }
+
+            // 계단으로 들어갈 준비가 된 사람은
+            // 계단에 3명 미만으로 있다면 movingQueue 에 push, 아니면 waitingQueue 에 push
             while (!stepsPq.isEmpty() && stepsPq.peek().arrivalTime + 1 <= currentTime) {
                 Person arrivedPerson = stepsPq.poll();
                 if (movingQueue.size() < 3) {
@@ -139,6 +162,7 @@ public class Solution {
                     ", startMovingTime=" + startMovingTime +
                     '}';
         }
+
     }
 
     private static class Steps {
@@ -160,5 +184,6 @@ public class Solution {
                     ", length=" + length +
                     '}';
         }
+
     }
 }
